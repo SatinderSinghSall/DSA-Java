1e05067ef7a96eed79d5ace8329e521efd78be76
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-09-04 16:55:39 +0530

    Topic: Arrays Reverse an Array.

--- 13. ARRAYS (Part I)/Arrays/ReverseArray.java
+++ 13. ARRAYS (Part I)/Arrays/ReverseArray.java
@@ -18,9 +18,14 @@ public class ReverseArray {
 
     public static void main(String[] args) {
         int numbers[] = {2, 4, 6, 8, 10};
+        System.out.println("Array before reversing: ");
+        for(int i = 0; i < numbers.length; i ++) {
+            System.out.print(numbers[i] + " ");
+        }
         reverseArray(numbers);
-        for(int i = 0; i <= numbers[i]; i ++) {
-            System.out.println(numbers[i]);
+        System.out.println("\n\nArray after reversing: ");
+        for(int i = 0; i < numbers.length; i ++) {
+            System.out.print(numbers[i] + " ");
         }
     }
 }
