615fe4ddca2f379034787c5bd1294a934d28c386
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-09-12 19:35:10 +0530

    DSA: Print Subarrays.

--- 13. ARRAYS (Part I)/Arrays/SubArrays.java
+++ 13. ARRAYS (Part I)/Arrays/SubArrays.java
@@ -4,6 +4,7 @@ package Arrays;
 
 public class SubArrays {
     public static void PrintSubarray(int numbers[]) {
+        int total_subarray = 0;
         for(int i = 0; i < numbers.length; i ++) {
             int start = i;
             for(int j = i + 1; j < numbers.length; j ++) {
@@ -11,10 +12,12 @@ public class SubArrays {
                 for(int k = start; k <= end; k ++) {
                     System.out.print(numbers[k] + " ");
                 }
+                total_subarray ++;
                 System.out.println();
             }
             System.out.println();
         }
+        System.out.println("Total Subarray: " + total_subarray);
     }
 
     public static void main(String args[]) {
