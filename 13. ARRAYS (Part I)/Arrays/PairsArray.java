f993393252004ee40697ed5b29e4bb740cfcea74
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-09-12 17:53:24 +0530

    Pairs in an Array.

--- 13. ARRAYS (Part I)/Arrays/PairsArray.java
+++ 13. ARRAYS (Part I)/Arrays/PairsArray.java
@@ -4,13 +4,16 @@ package Arrays;
 
 public class PairsArray {
     public static void printArrayPairs(int numbers[]) {
+        int total_pairs = 0;
         for(int i = 0; i < numbers.length; i ++) {
             int current_no = numbers[i];
             for(int j = i + 1; j < numbers.length; j ++) {
-                System.out.print("[ " + current_no + ", " + numbers[j] + " ]");
+                System.out.print("[" + current_no + ", " + numbers[j] + "] ");
+                total_pairs ++;
             }
             System.out.println();
         }
+        System.out.println("Total Pairs: " + total_pairs + "\n");
     }
 
     public static void main(String[] args) {
