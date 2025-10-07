b6210a260d443daad8d6b40248fceadd715c099b
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-10-07 23:13:53 +0530

    DSA: Insertion Sort Algorithm.

--- 15. Basic Sorting Algorithms/Sorting_Algorithms/InsertionSortAlgorithm.java
+++ 15. Basic Sorting Algorithms/Sorting_Algorithms/InsertionSortAlgorithm.java
@@ -26,9 +26,15 @@ public class InsertionSortAlgorithm {
     }
 
     public static void main(String[] args) {
+
         int arr[] = {5, 4, 1, 3, 2};
 
+        System.out.println("\nArray before Sorting:");
+        PrintArray(arr);
+
         InsertionSort_Algo(arr);
+
+        System.out.println("\nArray after Sorting:");
         PrintArray(arr);
     }
 }
