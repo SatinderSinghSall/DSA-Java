333f745796c33bdb9613f2a68dfe0357cce8fa2a
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-11-03 23:40:56 +0530

    DSA: String in Java.

---
+++ 17. Strings/Strings/src/JavaString.java
@@ -0,0 +1,25 @@
+// String in Java:
+
+import java.util.Scanner;
+
+public class JavaString {
+    public static void main(String[] args) {
+        char arr[] = {'a', 'b', 'c', 'd'};
+        String str = "a b c d";
+        String str2 = new String("x y z");
+        // Strings are immutable in Java.
+
+        // Strings - Input & Output:
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String userInput = scanner.nextLine();
+
+        System.out.println("\nUser Prompt: " + userInput);
+
+        // String - length()
+        String fullName = "Satinder Singh Sall";
+        System.out.println(fullName.length());
+
+        // String Concatenation:
+    }
+}
