e88dbc55a47d4b7833bac112982849447fd1767f
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-11-09 19:11:06 +0530

    DSA: Algorithms.

---
+++ 10. Patterns (Part I)/SquarePattern_V.java
@@ -0,0 +1,15 @@
+// Square Pattern - Java:
+
+public class SquarePattern_V {
+    public static void main(String[] args) {
+        int rows = 3;
+        int columns = 3;
+
+        for(int i = 1; i <= rows; i ++) {
+            for(int j = 1; j <= columns; j ++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
