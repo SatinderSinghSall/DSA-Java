f4118e72377656e2465990e2f066b81f621f69ad
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-05 21:39:39 +0530

    DSA: Array Data Structure - Arrays as Functions Arguments.

--- 13. ARRAYS (Part I)/Arrays_Part_2/Arrays_Functions.java
+++ 13. ARRAYS (Part I)/Arrays_Part_2/Arrays_Functions.java
@@ -3,7 +3,26 @@
 package Arrays_Part_2;
 
 public class Arrays_Functions {
+    public static void updateNumber(int marks[]) {
+        for(int i = 0; i < marks.length; i ++) {
+            marks[i] = marks[i] + 1;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Testing...");
+        int marks[] = {97, 98, 99};
+
+        System.out.println("Marks Before Update:");
+        for(int i = 0; i < marks.length; i ++) {
+            System.out.println("Marks: " + marks[i]);
+        }
+
+        updateNumber(marks);
+
+        System.out.println("\nMarks After Update:");
+        for(int i = 0; i < marks.length; i ++) {
+            System.out.println("Marks: " + marks[i]);
+        }
+        System.out.println();
     }
 }
