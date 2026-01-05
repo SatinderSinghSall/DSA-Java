ca5b1c0cd340c2dc97aa655142b6e595f57fc96e
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-05 21:40:59 +0530

    DSA: Array Data Structure - Linear Search Algorithm.

---
+++ 13. ARRAYS (Part I)/Arrays_Part_2/LinearSearch.java
@@ -0,0 +1,9 @@
+// Linear Search Algorithm:
+
+package Arrays_Part_2;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        System.out.println("Testing...");
+    }
+}
