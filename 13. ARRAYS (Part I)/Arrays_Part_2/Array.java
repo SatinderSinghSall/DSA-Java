182a1bf5082a49dc3a661cc48910858a2a7855a7
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-05 20:35:51 +0530

    DSA: Array Data Structure - Array Creation.

--- 13. ARRAYS (Part I)/Arrays_Part_2/Array.java
+++ 13. ARRAYS (Part I)/Arrays_Part_2/Array.java
@@ -3,6 +3,9 @@ package Arrays_Part_2;
 public class Array {
     public static void main(String[] args) {
         // Array Creation:
-        System.out.println("Testing...");
+        int marks[] = new int[50];
+        int numbers[] = {1, 2, 3};
+        int moreNumbers[] = {4, 5, 6};
+        String fruts[] = {"Apple", "Mango", "Orange"};
     }
 }
