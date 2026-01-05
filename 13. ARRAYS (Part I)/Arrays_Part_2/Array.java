b71202626821d94a8e531dea6c45b0f8cb1c1b4a
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-05 21:11:31 +0530

    DSA: Array Data Structure - Array Input / Output.

--- 13. ARRAYS (Part I)/Arrays_Part_2/Array.java
+++ 13. ARRAYS (Part I)/Arrays_Part_2/Array.java
@@ -1,5 +1,7 @@
 package Arrays_Part_2;
 
+import java.util.Scanner;
+
 public class Array {
     public static void main(String[] args) {
         // Array Creation:
@@ -7,5 +9,27 @@ public class Array {
         int numbers[] = {1, 2, 3};
         int moreNumbers[] = {4, 5, 6};
         String fruts[] = {"Apple", "Mango", "Orange"};
+
+        // Array Input / Output:
+        int arrMarks[] = new int[100];
+        Scanner sc = new Scanner(System.in);
+//        int phyics;
+//        phy = sc.nextInt();
+
+        System.out.print("Enter Physics marks: ");
+        arrMarks[0] = sc.nextInt(); // phy
+
+        System.out.print("Enter Chemistry marks: ");
+        arrMarks[1] = sc.nextInt(); // che
+
+        System.out.print("Enter Mathematics marks: ");
+        arrMarks[2] = sc.nextInt(); // mat
+
+        System.out.println("Marks of Physics: " + arrMarks[0]);
+        System.out.println("Marks of Chemistry: " + arrMarks[1]);
+        System.out.println("Marks of Mathematics: " + arrMarks[2]);
+
+        arrMarks[2] = 100;
+        System.out.println("Marks of Mathematics: " + arrMarks[2]);
     }
 }
