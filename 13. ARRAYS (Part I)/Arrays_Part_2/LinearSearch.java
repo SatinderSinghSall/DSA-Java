b3d693fa633664e86f9cf519b36c60332c636c42
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-06 09:54:53 +0530

    DSA: Array Data Structure - Implemented Linear Search Algorithm.

--- 13. ARRAYS (Part I)/Arrays_Part_2/LinearSearch.java
+++ 13. ARRAYS (Part I)/Arrays_Part_2/LinearSearch.java
@@ -3,7 +3,25 @@
 package Arrays_Part_2;
 
 public class LinearSearch {
+    public static void LinearSearch_Algorithm(int numbers[], int key) {
+        boolean found = false;
+        for(int i = 0; i < numbers.length; i ++) {
+            if(numbers[i] == key) {
+                System.out.println("Element " + key + " found at index: " + i);
+                found = true;
+                break;
+            }
+        }
+
+        if(!found) {
+            System.out.println("Key NOT found.");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Testing...");
+        int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
+        int key = 10;
+
+        LinearSearch_Algorithm(numbers, key);
     }
 }
