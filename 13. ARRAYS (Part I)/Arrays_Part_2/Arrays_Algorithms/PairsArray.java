3ff48ee90643f16063ed6bfb3c4ed370f6c90de8
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-07 22:47:01 +0530

    DSA: Arrays Data Structure - Pairs in Array.

---
+++ 13. ARRAYS (Part I)/Arrays_Part_2/Arrays_Algorithms/PairsArray.java
@@ -0,0 +1,13 @@
+// Pairs in Array:
+
+package Arrays_Part_2.Arrays_Algorithms;
+
+public class PairsArray {
+    public static void PairsArray_Logic() {
+        System.out.println("Testing...");
+    }
+
+    public static void main(String[] args) {
+        PairsArray_Logic();
+    }
+}
