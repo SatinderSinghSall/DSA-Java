7281f50f40093bb10981098d7aede37ab8d74b98
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-07 22:22:25 +0530

    DSA: Arrays Data Structure - Binary Search Algorithm.

--- 13. ARRAYS (Part I)/Arrays_Part_2/Arrays_Algorithms/BinarySearch.java
+++ 13. ARRAYS (Part I)/Arrays_Part_2/Arrays_Algorithms/BinarySearch.java
@@ -3,7 +3,32 @@
 package Arrays_Part_2.Arrays_Algorithms;
 
 public class BinarySearch {
+    public static int BinarySearch_Algorithm(int numbers[], int key) {
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            // Comparisons:
+            if (numbers[mid] == key) { // found
+                return mid;
+            } if (numbers[mid] < key) { // right
+                start = mid + 1;
+            } else { // left
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Testing...");
+        int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
+        int key = 10;
+
+        int index = BinarySearch_Algorithm(numbers, key);
+
+        System.out.println("Found the key " + key + " at index: " + index);
     }
 }
