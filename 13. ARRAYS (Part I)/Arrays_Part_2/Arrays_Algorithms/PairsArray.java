d49160760cec0a4d0142b03c2c506ff7626a7f59
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-07 23:19:59 +0530

    DSA: Arrays Data Structure - Pairs in Array.

--- 13. ARRAYS (Part I)/Arrays_Part_2/Arrays_Algorithms/PairsArray.java
+++ 13. ARRAYS (Part I)/Arrays_Part_2/Arrays_Algorithms/PairsArray.java
@@ -3,11 +3,19 @@
 package Arrays_Part_2.Arrays_Algorithms;
 
 public class PairsArray {
-    public static void PairsArray_Logic() {
-        System.out.println("Testing...");
+    public static void PairsArray_Logic(int numbers[]) {
+        for(int i = 0; i < numbers.length; i ++) {
+            int curr = numbers[i]; // 2, 4, 6, 8, 10
+            for(int j = i + 1; j < numbers.length; j ++) {
+                System.out.print("(" + curr + "," + numbers[j] + ")");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        PairsArray_Logic();
+        int numbers[] = {2, 4, 6, 8, 10};
+
+        PairsArray_Logic(numbers);
     }
 }
