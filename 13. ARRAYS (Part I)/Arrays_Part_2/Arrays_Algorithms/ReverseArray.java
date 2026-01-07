3db58a72c8617ab5bdbe5b26b49fbec25df10e23
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-07 22:43:36 +0530

    DSA: Arrays Data Structure - Reverse an Array.

--- 13. ARRAYS (Part I)/Arrays_Part_2/Arrays_Algorithms/ReverseArray.java
+++ 13. ARRAYS (Part I)/Arrays_Part_2/Arrays_Algorithms/ReverseArray.java
@@ -4,11 +4,29 @@ package Arrays_Part_2.Arrays_Algorithms;
 
 public class ReverseArray {
 
-    public static void ReverseArray_Logic() {
-        System.out.println("Testing...");
+    public static void ReverseArray_Logic(int numArr[]) {
+        int first = 0;
+        int last = numArr.length - 1;
+
+        while (first < last) {
+            // Swap the number: first & lase
+            int temp = numArr[last];
+            numArr[last] = numArr[first];
+            numArr[first] = temp;
+
+            first ++;
+            last --;
+        }
     }
 
     public static void main(String[] args) {
-        ReverseArray_Logic();
+        int numArr[] = {1, 4, 6, 8, 10};
+
+        ReverseArray_Logic(numArr);
+
+        for(int i = 0; i < numArr.length; i ++) {
+            System.out.println(numArr[i]);
+        }
+        System.out.println();
     }
 }
