db043629b1d1cc9a5fb8db2848fea4314745828d
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-07 22:11:11 +0530

    DSA: Arrays Data Structure - Binary Search Algorithm.

---
+++ 13. ARRAYS (Part I)/Arrays_Part_2/Arrays_Algorithms/BinarySearch.java
@@ -0,0 +1,9 @@
+// Binary Search Algorithm:
+
+package Arrays_Part_2.Arrays_Algorithms;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        System.out.println("Testing...");
+    }
+}
