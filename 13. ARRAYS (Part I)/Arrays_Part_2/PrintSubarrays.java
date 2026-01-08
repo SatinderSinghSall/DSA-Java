ddb15c2126ce403bf2e02d40700bb296922d2c90
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-08 23:13:56 +0530

    DSA: Arrays Data Structure - Printing Subarrays in a Array.

---
+++ 13. ARRAYS (Part I)/Arrays_Part_2/PrintSubarrays.java
@@ -0,0 +1,13 @@
+// Printing Subarrays in a Array:
+
+/*
+    - A continuous part of array.
+*/
+
+package Arrays_Part_2;
+
+public class PrintSubarrays {
+    public static void main(String[] args) {
+        System.out.println("Testing.,.,");
+    }
+}
