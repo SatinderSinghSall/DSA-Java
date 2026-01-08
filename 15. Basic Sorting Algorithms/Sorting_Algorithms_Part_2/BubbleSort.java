2899c1e6e5a7f3798b1371cd0214c150c1836191
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-09 00:02:32 +0530

    DSA: Sorting Algorithms - Bubble Sort Algorithm.

--- 15. Basic Sorting Algorithms/Sorting_Algorithms_Part_2/BubbleSort.java
+++ 15. Basic Sorting Algorithms/Sorting_Algorithms_Part_2/BubbleSort.java
@@ -15,13 +15,22 @@ public class BubbleSort {
         }
     }
 
-    public static void main(String[] args) {
-        int numbers[] = {5, 4, 1, 3, 2};
-
-        BubbleSort_Algorithm(numbers);
-
+    public static void PrintArray(int numbers[]) {
         for(int i = 0; i < numbers.length; i ++) {
             System.out.print(numbers[i] + " ");
         }
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {5, 4, 1, 3, 2};
+
+        System.out.println("Array before Sorting:");
+        PrintArray(numbers);
+
+        BubbleSort_Algorithm(numbers);
+
+        System.out.println();
+        System.out.println("\nArray after Sorting:");
+        PrintArray(numbers);
     }
 }
