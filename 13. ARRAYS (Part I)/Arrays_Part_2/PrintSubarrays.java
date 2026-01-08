36a40e0a7e4129d757e6a3e0d75403256aa4ffb0
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-08 23:20:29 +0530

    DSA: Arrays Data Structure - Printing Subarrays in a Array.

--- 13. ARRAYS (Part I)/Arrays_Part_2/PrintSubarrays.java
+++ 13. ARRAYS (Part I)/Arrays_Part_2/PrintSubarrays.java
@@ -7,7 +7,13 @@
 package Arrays_Part_2;
 
 public class PrintSubarrays {
+    public static void  Subarray_Logic(int numArr[]) {
+        System.out.println("Testing...");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Testing.,.,");
+        int numArr[] = {2, 4, 6, 8, 10};
+
+        Subarray_Logic(numArr);
     }
 }
