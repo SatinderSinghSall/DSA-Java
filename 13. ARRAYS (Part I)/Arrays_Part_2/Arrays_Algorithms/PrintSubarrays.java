32707901469b2e6610b5f550d213addf39c9a564
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-08 23:44:56 +0530

    DSA: Sorting Algorithms - Bubble Sort Algorithm.

---
+++ 13. ARRAYS (Part I)/Arrays_Part_2/Arrays_Algorithms/PrintSubarrays.java
@@ -0,0 +1,33 @@
+// Printing Subarrays in a Array:
+
+/*
+    - A continuous part of array.
+*/
+
+package Arrays_Part_2.Arrays_Algorithms;
+
+public class PrintSubarrays {
+    public static void  Subarray_Logic(int numArr[]) {
+        int totalSubarrays = 0;
+        for(int i = 0; i < numArr.length; i ++) {
+            int start = i;
+            for(int j = i; j < numArr.length; j ++) {
+                int end = j;
+                for(int k = start; k <= end; k ++) {
+                    System.out.print(numArr[k] + " ");
+                }
+                System.out.println();
+                totalSubarrays = totalSubarrays + 1;
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("Total Subarrays: " + totalSubarrays);
+    }
+
+    public static void main(String[] args) {
+        int numArr[] = {2, 4, 6, 8, 10};
+
+        Subarray_Logic(numArr);
+    }
+}
---
+++ 15. Basic Sorting Algorithms/Sorting_Algorithms_Part_2/BubbleSort.java
@@ -0,0 +1,15 @@
+// Bubble Sort Algorithm:
+
+package Sorting_Algorithms_Part_2;
+
+public class BubbleSort {
+    public static void BubbleSort_Algorithm(int numbers[]) {
+        System.out.println("Testing...");
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 6, 8, 10};
+
+        BubbleSort_Algorithm(numbers);
+    }
+}
