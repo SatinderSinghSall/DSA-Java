5e9dc097dcbeeed6b458058e07fd09e5a2ed2a1b
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-09 23:24:02 +0530

    DSA: 2D Arrays - Creating a 2D Array.

--- 16. 2D Arrays/Arrays_2D_Part_2/Creating_2D_Array.java
+++ 16. 2D Arrays/Arrays_2D_Part_2/Creating_2D_Array.java
@@ -5,10 +5,24 @@ package Arrays_2D_Part_2;
 import java.util.Scanner;
 
 public class Creating_2D_Array {
+    public static boolean SearchElement(int matrix[][], int key) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for(int i = 0; i < n; i ++) {
+            for(int j = 0; j < m; j ++) {
+                if(matrix[i][j] == key) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int matrix[][] = new int[3][3];
         int n = matrix.length; // row
         int m = matrix[0].length; // cols
+        int key = 10;
 
         Scanner sc = new Scanner(System.in);
 
@@ -25,6 +39,14 @@ public class Creating_2D_Array {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
+        }
+
+        boolean searchResult = SearchElement(matrix, key);
+
+        if(searchResult == true) {
+            System.out.println("Element " + key + " found.");
+        } else {
+            System.out.println("Element " + key + " not found.");
         }
     }
 }
