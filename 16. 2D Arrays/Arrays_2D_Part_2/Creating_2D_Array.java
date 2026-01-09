83757181b843beb6e78b7b43aee2df182bf5913b
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-09 22:54:37 +0530

    DSA: 2D Arrays - Creating a 2D Array.

---
+++ 16. 2D Arrays/Arrays_2D_Part_2/Creating_2D_Array.java
@@ -0,0 +1,9 @@
+// Creating a 2D Array:
+
+package Arrays_2D_Part_2;
+
+public class Creating_2D_Array {
+    public static void main(String[] args) {
+        System.out.println("Testing...2D Array.");
+    }
+}
