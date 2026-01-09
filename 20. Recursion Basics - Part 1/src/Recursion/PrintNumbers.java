5594edb5af9f78938f6b046d772166771bffbb95
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-10 01:35:19 +0530

    DSA: Recursion - Basic Recursion.

---
+++ 20. Recursion Basics - Part 1/src/Recursion/PrintNumbers.java
@@ -0,0 +1,17 @@
+/*
+    == Problem Statement: ==
+    Print numbers in decreasing and increasing order
+    from n to 1 and 1 to n.
+*/
+
+package Recursion;
+
+public class PrintNumbers {
+    public static void printNumberDescending() {
+        System.out.println("Testing...");
+    }
+
+    public static void main(String[] args) {
+        printNumberDescending();
+    }
+}
