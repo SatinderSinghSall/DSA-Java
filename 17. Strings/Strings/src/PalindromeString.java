9af39da9af047a5c62ec847b1f57699ca04a2d00
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-10 00:30:20 +0530

    DSA: Strings - Palindrome String.

--- 17. Strings/Strings/src/PalindromeString.java
+++ 17. Strings/Strings/src/PalindromeString.java
@@ -1,7 +1,18 @@
 // Problem: Palindrome String:
 
 public class PalindromeString {
+    public static boolean isPalandrome(String str) {
+        for(int i = 0; i < str.length() / 2; i ++) {
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Testing...");
+        String str = "Maam";
+        System.out.println(isPalandrome(str));;
     }
 }
