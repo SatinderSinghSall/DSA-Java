dab439a00698d8029a088e50953993b632d76ef0
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-09 22:27:03 +0530

    DSA: Sorting Algorithms - Selection Sort Algorithm.

--- 15. Basic Sorting Algorithms/Sorting_Algorithms_Part_2/SelectionSort.java
+++ 15. Basic Sorting Algorithms/Sorting_Algorithms_Part_2/SelectionSort.java
@@ -4,7 +4,17 @@ package Sorting_Algorithms_Part_2;
 
 public class SelectionSort {
     public static void SelectionSort_Algorithm(int numbers[]) {
-        System.out.println("Testing...");
+        for(int i =0; i < numbers.length - 1; i ++) {
+            int minPos = i;
+            for(int j = i + 1; j < numbers.length; j ++) {
+                if(numbers[minPos] > numbers[j]) {
+                    minPos = j;
+                }
+            }
+            int temp = numbers[minPos];
+            numbers[minPos] = numbers[i];
+            numbers[i] = temp;
+        }
     }
 
     public static void PrintArray(int numbers[]) {
