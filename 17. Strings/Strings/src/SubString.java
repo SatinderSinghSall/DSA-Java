4401d2dfc4a44a02707fe3cc8013bc1ec9d81b7b
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-10 01:08:04 +0530

    DSA: Strings - Substring in Java.

---
+++ 17. Strings/Strings/src/SubString.java
@@ -0,0 +1,20 @@
+// Substring in Java:
+
+public class SubString {
+    public static String SubString(String str, int si, int ei) {
+        String subStr = "";
+        for(int i = 0; i < ei; i ++) {
+            subStr = subStr + str.charAt(i);
+        }
+
+        return subStr;
+    }
+
+    public static void main(String[] args) {
+        String str = "HelloWorld";
+        int si = 0;
+        int ei = 5;
+
+        System.out.println(SubString(str, si, ei));
+    }
+}
