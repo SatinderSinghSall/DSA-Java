f66a1844ec531134d3f64b11f474fe1f69d44e6c
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-09 23:12:24 +0530

    DSA: 2D Arrays - Creating a 2D Array.

--- 16. 2D Arrays/Arrays_2D_Part_2/Creating_2D_Array.java
+++ 16. 2D Arrays/Arrays_2D_Part_2/Creating_2D_Array.java
@@ -2,8 +2,29 @@
 
 package Arrays_2D_Part_2;
 
+import java.util.Scanner;
+
 public class Creating_2D_Array {
     public static void main(String[] args) {
-        System.out.println("Testing...2D Array.");
+        int matrix[][] = new int[3][3];
+        int n = matrix.length; // row
+        int m = matrix[0].length; // cols
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter data for matrix " + n + " x " + m + ": ");
+        for(int i = 0; i < n; i ++) {
+            for(int j = 0; j < m; j ++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("\nArray is:");
+        for(int i = 0; i < n; i ++) {
+            for(int j = 0; j < m; j ++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
