65d1585e8556f4d5d78ab2d51dec9db8e34eda8f
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-10 22:22:28 +0530

    DSA: Recursion - Basic Recursion.

--- 20. Recursion Basics - Part 1/src/ElementOccurrence.java
+++ 20. Recursion Basics - Part 1/src/ElementOccurrence.java
@@ -1,8 +1,14 @@
 // Check an element occurrence first & last using Recursion:
 
 public class ElementOccurrence {
-    public static void FirstCheck_Logic() {
-        System.out.println("Testing...");
+    public static int FirstCheck_Logic(int arr[], int key, int index) {
+        if (index == arr.length - 1) {
+            return -1;
+        }
+        if (arr[index] == key) {
+           return index;
+       }
+       return FirstCheck_Logic(arr, key, index + 1);
     }
 
     public static void LastCheck_Logic() {
@@ -10,8 +16,12 @@ public class ElementOccurrence {
     }
 
     public static void main(String[] args) {
-        FirstCheck_Logic();
+        int arr[] = {8, 3, 6, 9, 5, 10, 2, 5, 3};
+        int key = 5;
+        int index = 0;
 
-        LastCheck_Logic();
+        System.out.println(FirstCheck_Logic(arr, key, index));
+
+//        LastCheck_Logic();
     }
 }
