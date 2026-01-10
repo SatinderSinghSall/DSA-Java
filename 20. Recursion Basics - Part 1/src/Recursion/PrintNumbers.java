42fdc7c1e8be54c7801d534761ea70d617d374c0
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-10 20:34:02 +0530

    DSA: Recursion - Basic Recursion.

--- 20. Recursion Basics - Part 1/src/Recursion/PrintNumbers.java
+++ 20. Recursion Basics - Part 1/src/Recursion/PrintNumbers.java
@@ -9,16 +9,26 @@ package Recursion;
 public class PrintNumbers {
     public static void printNumberDescending(int num) {
         if(num == 1) { // base-case
-            System.out.println(1);
+            System.out.print(1);
             return;
         }
         System.out.print(num + " ");
         printNumberDescending(num - 1);
     }
 
+    public static void printNumberIncreasing(int num) {
+        if(num == 0) { // base-case
+            return;
+        }
+        printNumberIncreasing(num - 1);
+        System.out.print(num + " ");
+    }
+
     public static void main(String[] args) {
         int num = 10;
 
         printNumberDescending(num);
+        System.out.println();
+        printNumberIncreasing(num);
     }
 }
