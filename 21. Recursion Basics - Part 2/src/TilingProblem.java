e704dcd994f3694690229f13c9789219e67b076a
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-10 22:26:15 +0530

    DSA: Recursion - Tiling Problem.

---
+++ 21. Recursion Basics - Part 2/src/TilingProblem.java
@@ -0,0 +1,11 @@
+// Tiling Problem:
+
+public class TilingProblem {
+    public static void Tiling_Logic() {
+        System.out.println("Testing...");
+    }
+
+    public static void main(String[] args) {
+        Tiling_Logic();
+    }
+}
