5711ea24bb2ea2373c528771174a7ce21d9d7686
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-10 22:37:12 +0530

    DSA: Recursion - Tiling Problem.

--- 21. Recursion Basics - Part 2/src/TilingProblem.java
+++ 21. Recursion Basics - Part 2/src/TilingProblem.java
@@ -1,5 +1,13 @@
 // Tiling Problem:
 
+/*
+ == Problem Statement: ==
+ * Given a 2 × n board and tiles of size 2 × 1, find the total number of
+ * ways to completely fill the board using these tiles. Each tile can be
+ * placed either horizontally i.e., as a 1 x 2 tile or vertically i.e.,
+ * as 2 x 1 tile.
+*/
+
 public class TilingProblem {
     public static void Tiling_Logic() {
         System.out.println("Testing...");
