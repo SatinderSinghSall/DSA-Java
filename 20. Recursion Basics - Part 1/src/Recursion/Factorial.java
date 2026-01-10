af38e670487ed4d161ebca6ddea1387fc3f893ea
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-10 20:56:43 +0530

    DSA: Recursion - Basic Recursion.

---
+++ 20. Recursion Basics - Part 1/src/Recursion/Factorial.java
@@ -0,0 +1,17 @@
+/*
+    == Problem Statement: ==
+    Find factorial of n.
+*/
+
+package Recursion;
+
+public class Factorial {
+    public static void Factorial_Logic(int num) {
+        System.out.println(num);
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        Factorial_Logic(num);
+    }
+}
