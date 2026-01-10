c2a12790dad799bd1dae9345fa2e72250362465a
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-10 21:20:31 +0530

    DSA: Recursion - Basic Recursion.

--- 20. Recursion Basics - Part 1/src/NaturalNumbersSum.java
+++ 20. Recursion Basics - Part 1/src/NaturalNumbersSum.java
@@ -12,10 +12,21 @@ public class NaturalNumbersSum {
         return n + sum(n - 1);
     }
 
+    public static int sum2(int n) {
+        if(n == 1) {
+            return 1;
+        }
+        int snm1 = sum2(n - 1);
+        int sn = n + snm1;
+        return sn;
+    }
+
     public static void main(String[] args) {
         int n = 10;  // You can change this value
         int result = sum(n);
 
         System.out.println("Sum of first " + n + " natural numbers is: " + result);
+
+        System.out.println(sum2(n));
     }
 }
