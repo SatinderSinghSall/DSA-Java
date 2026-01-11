3acce6faafbc10b735d35c69eba0a040dddc9102
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-12 00:23:11 +0530

    DSA: Recursion - Binary String Problem.

---
+++ 21. Recursion Basics - Part 2/src/BinaryStringProblem.java
@@ -0,0 +1,11 @@
+// Binary String Problem:
+
+public class BinaryStringProblem {
+    public static void BinaryStringProblem_Logic() {
+        System.out.println("Testing...");
+    }
+
+    public static void main(String[] args) {
+        BinaryStringProblem_Logic();
+    }
+}
