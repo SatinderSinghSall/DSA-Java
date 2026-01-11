5fe5930137f94c28eb08adafb6461ac04bdda1dd
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-11 23:41:58 +0530

    DSA: Recursion - Remove Duplicate String Problem.

--- 21. Recursion Basics - Part 2/src/RemoveDuplicateString.java
+++ 21. Recursion Basics - Part 2/src/RemoveDuplicateString.java
@@ -1,11 +1,23 @@
 // Remove Duplicate String:
 
 public class RemoveDuplicateString {
-    public static void RemoveDuplicateString_Logic() {
-        System.out.println("Testing...");
+    public static void RemoveDuplicateString_Logic(String str, int idx, StringBuilder newString, boolean map[]) {
+        if (idx == str.length()) {
+            System.out.println(newString);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+        if (map[currChar - 'a'] == true) {
+            RemoveDuplicateString_Logic(str, idx + 1, newString, map);
+        } else {
+            map[currChar - 'a'] = true;
+            RemoveDuplicateString_Logic(str, idx + 1, newString.append(currChar), map);
+        }
     }
 
     public static void main(String[] args) {
-        RemoveDuplicateString_Logic();
+        String str = "apnacollege";
+        RemoveDuplicateString_Logic(str, 0, new StringBuilder(""), new boolean[26]);
     }
 }
