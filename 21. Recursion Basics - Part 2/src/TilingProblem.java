49ee42a4a3326f588f48feaae634dc46afff5b2b
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-11 22:42:52 +0530

    DSA: Recursion - Tiling Problem.

--- 21. Recursion Basics - Part 2/src/TilingProblem.java
+++ 21. Recursion Basics - Part 2/src/TilingProblem.java
@@ -9,13 +9,24 @@
 */
 
 public class TilingProblem {
-    public static int Tiling_Problem_Logic() {
-        System.out.println("Testing...");
+    public static int Tiling_Problem_Logic(int n) { // 2 x n (floor size)
+        if (n == 0 || n == 1) {
+            return 1;
+        }
 
-        return 0;
+        // vertical choice:
+        int fnm1 = Tiling_Problem_Logic(n - 1);
+
+        // horizontal choice:
+        int fnm2 = Tiling_Problem_Logic(n - 2);
+
+        int total_ways = fnm1 + fnm2;
+
+        return total_ways;
     }
 
     public static void main(String[] args) {
-        Tiling_Problem_Logic();
+        int n = 4;
+        System.out.println(Tiling_Problem_Logic(n));
     }
 }
