d3ab518d1f43a670b1753d77ba3a3116b6153475
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-12 22:27:56 +0530

    DSA: Divide and Conquer Algorithms - Merge Sort Algorithm.

---
+++ 22. Divide and Conquer/Divide_and_Conquer/src/MergeSort.java
@@ -0,0 +1,11 @@
+// Merge Sort Algorithm:
+
+public class MergeSort {
+    public static void MergeSort_Algorithm() {
+        System.out.println("Testing...");
+    }
+
+    public static void main(String[] args) {
+        MergeSort_Algorithm();
+    }
+}
