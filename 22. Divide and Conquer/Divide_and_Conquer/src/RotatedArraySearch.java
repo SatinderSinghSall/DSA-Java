605e64d0a9e2a02a31a38cdb2311cd765b5a0c38
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-14 22:23:54 +0530

    DSA: Divide and Conquer Algorithms - Rotated Array Search Algorithm.

--- 22. Divide and Conquer/Divide_and_Conquer/src/RotatedArraySearch.java
+++ 22. Divide and Conquer/Divide_and_Conquer/src/RotatedArraySearch.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+// Rotated Array Search Algorithm:
 
 public class RotatedArraySearch {
+    public static void PrintArray(int arr[]) {
+        for(int i = 0; i < arr.length; i ++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void RotatedArraySearch_Algorithm(int arr[]) {
+        System.out.println("Testing...");
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {4, 5, 6, 7, 0, 1, 2};
+
+        PrintArray(arr);
+        RotatedArraySearch_Algorithm(arr);
+        PrintArray(arr);
+    }
 }
