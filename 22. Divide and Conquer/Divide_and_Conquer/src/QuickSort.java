1d06843a0cd098ba00af885c56602cf45af6c75b
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-14 22:18:05 +0530

    DSA: Divide and Conquer Algorithms - Quick Sort Algorithm.

--- 22. Divide and Conquer/Divide_and_Conquer/src/QuickSort.java
+++ 22. Divide and Conquer/Divide_and_Conquer/src/QuickSort.java
@@ -8,8 +8,32 @@ public class QuickSort {
         System.out.println();
     }
 
-    public static void QuickSort_Algorithm(int arr[], int si, int ei) {
+    public static int partition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+        int i = si - 1;
+        for (int j = si; j < ei; j ++) {
+            if (arr[j] <= pivot) {
+                i ++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i ++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
 
+    public static void QuickSort_Algorithm(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+
+        int p_index = partition(arr, si, ei); // last element
+        QuickSort_Algorithm(arr, si, p_index - 1); // left part
+        QuickSort_Algorithm(arr, p_index + 1, ei); // right part
     }
 
     public static void main(String[] args) {
