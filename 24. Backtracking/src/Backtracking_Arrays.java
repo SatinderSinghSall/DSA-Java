9a3a7a039c7d7690c956e867ae1a74dc3d2f644e
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-21 21:39:47 +0530

    DSA: Backtracking & Time and Space Complexity.

--- 24. Backtracking/src/Backtracking_Arrays.java
+++ 24. Backtracking/src/Backtracking_Arrays.java
@@ -1,7 +1,24 @@
 // Backtracking on Arrays:
 
 public class Backtracking_Arrays {
+    public static void printArray(int arr[]) {
+        for(int i = 0; i < arr.length; i ++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void changeArray(int arr[], int index, int value) {
+        System.out.println("Testing...");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Testng...");
+        int arr[] = new int[5];
+        int index = 0;
+        int value = 1;
+
+        changeArray(arr, index, value);
+        printArray(arr);
+
     }
 }
