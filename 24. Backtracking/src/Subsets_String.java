42ed70d51fcb5368e6e8bb767cee99e8953ab416
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-01-25 20:40:52 +0530

    DSA: Backtracking - Subsets of a String.

--- 24. Backtracking/src/Subsets_String.java
+++ 24. Backtracking/src/Subsets_String.java
@@ -1,7 +1,24 @@
 // Subsets of a String:
 
 public class Subsets_String {
+    public static void findSubset(String str, String ans, int index) {
+        // base case:
+        if (index == str.length()) {
+            if (ans.length() == 0) {
+                System.out.println("NULL");
+            } else {
+                System.out.println(ans);
+            }
+            return;
+        }
+
+        // recursion step:
+        findSubset(str, ans + str.charAt(index), index + 1); // yes choice
+        findSubset(str, ans, index + 1); // no choice
+    }
+
     public static void main(String[] args) {
-        System.out.println("Testing...");
+        String str = "abc";
+        findSubset(str, "", 0);
     }
 }
