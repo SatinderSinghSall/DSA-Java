e6c90b9888946fc72e4bea33c65233003a75f7d2
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-02-03 21:24:12 +0530

    DSA: Permutations of a String.

--- 24. Backtracking/src/Permutations_String.java
+++ 24. Backtracking/src/Permutations_String.java
@@ -1,7 +1,25 @@
 // Permutations of a String:
 
 public class Permutations_String {
+    public static void FindPermutations(String str, String ans) {
+        // Base Case:
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        // Recursion:
+        for (int i = 0; i < str.length(); i ++) {
+            char curr = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+            FindPermutations(newStr, ans + curr);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Testing...");
+        String str = "abc";
+        String ans = "";
+
+        FindPermutations(str, ans);
     }
 }
