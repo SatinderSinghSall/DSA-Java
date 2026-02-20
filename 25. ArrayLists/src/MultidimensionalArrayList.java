4f1bf0791d36ddfd62fdb3b4f22d921baedf4e1c
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-02-20 16:45:09 +0530

    DSA: ArrayLists - Multidimensional ArrayList in Java.

--- 25. ArrayLists/src/MultidimensionalArrayList.java
+++ 25. ArrayLists/src/MultidimensionalArrayList.java
@@ -1,7 +1,31 @@
 // Multidimensional ArrayList in Java:
 
+import java.util.ArrayList;
+
 public class MultidimensionalArrayList {
     public static void main(String[] args) {
+        // Multidimensional ArrayList Creation:
+        ArrayList <ArrayList <Integer>> mainList = new ArrayList<>();
 
+        // List - 1:
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        mainList.add(list);
+        System.out.println(list);
+
+        // List - 2:
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(3);
+        list2.add(4);
+        mainList.add(list2);
+        System.out.println(list2);
+
+        for(int i = 0; i < mainList.size(); i ++) {
+            ArrayList<Integer> currList = mainList.get(i);
+            for(int j = 0; j < currList.size(); j ++) {
+                System.out.print(currList.get(j) + " ");
+            }
+        }
     }
 }
