fb35e92d31e3c7221d531e1b98eae602afdf3100
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-02-20 21:47:21 +0530

    DSA: ArrayLists - Pair Sum (Sorted) - 2 Pointers Approach.

---
+++ 25. ArrayLists/src/PairSum_2PointersApproach.java
@@ -0,0 +1,7 @@
+// Pair Sum (Sorted) - 2 Pointers Approach
+
+public class PairSum_2PointersApproach {
+    public static void main(String[] args) {
+        System.out.println("Testing...");
+    }
+}
--- 25. ArrayLists/src/PairSum_BruteForce.java
+++ 25. ArrayLists/src/PairSum_BruteForce.java
@@ -1,4 +1,4 @@
-// Pair Sum - Brute Force Approach
+// Pair Sum (Sorted) - Brute Force Approach
 
 public class PairSum_BruteForce {
     public static void main(String[] args) {
