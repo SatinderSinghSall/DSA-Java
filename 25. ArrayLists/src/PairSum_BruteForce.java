f2f78f5cc694e7c0a0f3e026eac7526f3110b483
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-02-20 21:54:10 +0530

    DSA: ArrayLists - Pair Sum - Brute Force Approach.

--- 25. ArrayLists/src/PairSum_BruteForce.java
+++ 25. ArrayLists/src/PairSum_BruteForce.java
@@ -1,7 +1,30 @@
 // Pair Sum (Sorted) - Brute Force Approach
 
-public class PairSum_BruteForce {
-    public static void main(String[] args) {
+import java.util.ArrayList;
 
+public class PairSum_BruteForce {
+    public static boolean PairSum(ArrayList<Integer> list, int target) {
+        for(int i = 0; i < list.size(); i ++) {
+            for(int j = i + 1; j < list.size(); j ++) {
+                if(list.get(i) + list.get(j) == target) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        int target = 5;
+
+        System.out.println(list);
+        System.out.println(PairSum(list, target));
     }
 }
