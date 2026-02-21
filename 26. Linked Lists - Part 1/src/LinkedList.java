edf4ced2485e798819e9942d4c6c33c90e0a9283
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-02-21 21:35:25 +0530

    DSA: Linked Lists Data Structure.

---
+++ 26. Linked Lists - Part 1/src/LinkedList.java
@@ -0,0 +1,17 @@
+// Linked List Data Structure:
+
+public class LinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Testing...main");
+    }
+}
