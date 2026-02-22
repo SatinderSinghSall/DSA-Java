de0c42c4075a9f82184f25a9f3d02d9b095f11e2
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-02-22 13:10:21 +0530

    DSA: Linked Lists Data Structure - Add Last in Linked List & Add first in Linked List.

--- 26. Linked Lists - Part 1/src/LinkedList.java
+++ 26. Linked Lists - Part 1/src/LinkedList.java
@@ -15,6 +15,7 @@ public class LinkedList {
     public static Node head;
     public static Node tail;
 
+    // Add first in Linked List.
     public void addFirst(int data) {
         // Step 1 - Create a new Node:
         Node newNode = new Node(data);
@@ -31,10 +32,29 @@ public class LinkedList {
         head = newNode;
     }
 
+    // Add Last in Linked List:
+    public void addLast(int data) {
+        // Step 1 - Create a new Node:
+        Node newNode = new Node(data);
+
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        // Step 2 - tail.Next - newNode:
+        tail.next = newNode;
+
+        // Step 3 - tail = newNode:
+        tail = newNode;
+    }
+
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
 
-        linkedList.addFirst(1);
         linkedList.addFirst(2);
+        linkedList.addFirst(1);
+        linkedList.addLast(3);
+        linkedList.addLast(4);
     }
 }
