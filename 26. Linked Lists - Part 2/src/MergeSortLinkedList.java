6504c4fb318875f58dce7172dcc0abc1b3083268
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-15 21:13:17 +0530

    DSA: Linked List Data Structure - Merge Sort on Linked List.

---
+++ 26. Linked Lists - Part 2/src/MergeSortLinkedList.java
@@ -0,0 +1,7 @@
+// Merge Sort on Linked List:
+
+public class MergeSortLinkedList {
+    public static void main(String[] args) {
+        System.out.println("Merge Sort on Linked List...");
+    }
+}
