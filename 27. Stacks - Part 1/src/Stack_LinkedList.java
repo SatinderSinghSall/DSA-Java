7020cc1f59fb97a1ab966a40fd93413301911373
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-19 21:18:01 +0530

    DSA: Stack Data Structure - Stack Data Structure: Using Linked List.

--- 27. Stacks - Part 1/src/Stack_LinkedList.java
+++ 27. Stacks - Part 1/src/Stack_LinkedList.java
@@ -1,7 +1,70 @@
 // Stack Data Structure: Using Linked List
 
 public class Stack_LinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Stack {
+        static Node head = null;
+
+        public static boolean isEmpty()  {
+            return head == null;
+        }
+
+        // Push Method:
+        public static void push(int data) {
+            Node newNode = new Node(data);
+
+            if (isEmpty()) {
+                head = newNode;
+                return;
+            }
+
+            newNode.next = head;
+            head = newNode;
+
+            return;
+        }
+
+        // Pop Method:
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+
+            return head.data;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Stack Data Structure: Using Linked List.");
+
+        Stack_ArrayList.Stack stack = new Stack_ArrayList.Stack();
+
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.Peek());
+            stack.Pop();
+        }
     }
 }
