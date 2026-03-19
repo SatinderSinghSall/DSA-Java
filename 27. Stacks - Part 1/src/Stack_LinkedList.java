da43574f37554bb5b7556948beae54ff232d80e5
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-19 19:55:11 +0530

    DSA: Stack Data Structure - Stack Data Structure: Using Linked List.

---
+++ 27. Stacks - Part 1/src/Stack_LinkedList.java
@@ -0,0 +1,7 @@
+// Stack Data Structure: Using Linked List
+
+public class Stack_LinkedList {
+    public static void main(String[] args) {
+        System.out.println("Stack Data Structure: Using Linked List.");
+    }
+}
