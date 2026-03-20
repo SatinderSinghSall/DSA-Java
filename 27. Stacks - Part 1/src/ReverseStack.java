019aa844a28aa7d0b1e2db2a9675f1d0a14dc1a1
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-20 18:57:48 +0530

    DSA: Stack Data Structure - Stack-Problem: Reverse a Stack.

---
+++ 27. Stacks - Part 1/src/ReverseStack.java
@@ -0,0 +1,7 @@
+// Stack-Problem: Reverse a Stack:
+
+public class ReverseStack {
+    public static void main(String[] args) {
+        System.out.println("Stack-Problem: Reverse a Stack");
+    }
+}
