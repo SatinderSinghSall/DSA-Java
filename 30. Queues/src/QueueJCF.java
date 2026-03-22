0e1a84621afde8ab9b2af0bf3a14ba3d7a517e5e
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-22 22:30:41 +0530

    DSA: Queue Data Structure - Queue: Implementing a Queue using Java Collection Framework. (JCF)

---
+++ 30. Queues/src/QueueJCF.java
@@ -0,0 +1,7 @@
+// Queue: Implementing a Queue using Java Collection Framework. (JCF)
+
+public class QueueJCF {
+    public static void main(String[] args) {
+        System.out.println("Queue: Implementing a Queue using Java Collection Framework. (JCF)");
+    }
+}
