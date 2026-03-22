74a3b9d904935b7cfef9004dd5822f47cb8f9f6d
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-22 21:28:02 +0530

    DSA: Queue Data Structure - Queue: Implementing a Queue using a Linked List..

---
+++ 30. Queues/src/QueueLinkedList.java
@@ -0,0 +1,7 @@
+// Queue: Implementing a Queue using a Linked List.
+
+public class QueueLinkedList {
+    public static void main(String[] args) {
+        System.out.println("Queue: Implementing a Queue using a Linked List.");
+    }
+}
