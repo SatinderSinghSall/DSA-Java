914fd4178b5c8473c3f374a00b2f19bb8b34c548
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-22 12:11:00 +0530

    DSA: Queue Data Structure - Queue: Implementing a Queue using an Array.

---
+++ 30. Queues/src/QueueArray.java
@@ -0,0 +1,7 @@
+// Queue: Implementing a Queue using an Array
+
+public class QueueArray {
+    public static void main(String[] args) {
+        System.out.println("Queue: Implementing a Queue using an Array.");
+    }
+}
