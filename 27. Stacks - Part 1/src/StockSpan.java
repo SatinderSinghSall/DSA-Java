3bebf010b697881cf5c6f3406a9318e1409a2347
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-22 10:13:09 +0530

    DSA: Stack Data Structure - Stack-Problem: Stock Span Problem.

---
+++ 27. Stacks - Part 1/src/StockSpan.java
@@ -0,0 +1,7 @@
+// Stack-Problem: Stock Span Problem.
+
+public class StockSpan {
+    public static void main(String[] args) {
+        System.out.println("Stack-Problem: Stock Span Problem.");
+    }
+}
