420f0cae4ec7df3ef609a49add9a5adf7d93db1b
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-22 22:37:45 +0530

    DSA: Queue Data Structure - Queue: Implementing a Queue using Java Collection Framework. (JCF)

--- 30. Queues/src/QueueJCF.java
+++ 30. Queues/src/QueueJCF.java
@@ -1,7 +1,27 @@
 // Queue: Implementing a Queue using Java Collection Framework. (JCF)
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class QueueJCF {
     public static void main(String[] args) {
         System.out.println("Queue: Implementing a Queue using Java Collection Framework. (JCF)");
+
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        System.out.println(q.remove());
+        q.add(4);
+
+        System.out.println(q.remove());
+        q.add(5);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
     }
 }
