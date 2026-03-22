57a4d1d279375f6b4c9fad144bb7788254cd9710
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-22 10:14:28 +0530

    DSA: Stack Data Structure - Stack-Problem: Next Greater Element Problem..

---
+++ 27. Stacks - Part 1/src/NextGreaterElement.java
@@ -0,0 +1,7 @@
+// Stack-Problem: Next Greater Element Problem.
+
+public class NextGreaterElement {
+    public static void main(String[] args) {
+        System.out.println("Stack-Problem: Next Greater Element Problem.");
+    }
+}
