231a870f4a607517ca5af886c70a5c6afde0a142
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-23 19:11:15 +0530

    DSA: Queue Data Structure - Queue: Implementing a Queue using 2 Stack.

---
+++ 30. Queues/src/Queue2Stack.java
@@ -0,0 +1,7 @@
+// Queue: Implementing a Queue using 2 Stack.
+
+public class Queue2Stack {
+    public static void main(String[] args) {
+        System.out.println("Queue: Implementing a Queue using 2 Stack.");
+    }
+}
