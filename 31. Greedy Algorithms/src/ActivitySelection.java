0252a7fcf5272b3194b6c278faa473569f8a73ce
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-24 21:12:04 +0530

    DSA: Greedy Algorithms - Greedy Algorithm Problem - Activity Selection..

---
+++ 31. Greedy Algorithms/src/ActivitySelection.java
@@ -0,0 +1,7 @@
+// Greedy Algorithm Problem - Activity Selection.
+
+public class ActivitySelection {
+    public static void main(String[] args) {
+        System.out.println("Greedy Algorithm Problem - Activity Selection.");
+    }
+}
