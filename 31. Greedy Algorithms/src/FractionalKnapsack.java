baa0b9584923e9eb2230edecfd2f38091f3fbebb
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-25 19:28:04 +0530

    DSA: Greedy Algorithms - Greedy Algorithm Problem - Fractional Knapsack.

--- 31. Greedy Algorithms/src/FractionalKnapsack.java
+++ 31. Greedy Algorithms/src/FractionalKnapsack.java
@@ -3,5 +3,9 @@
 public class FractionalKnapsack {
     public static void main(String[] args) {
         System.out.println("Greedy Algorithm Problem - Fractional Knapsack.");
+
+        int value[] = {60, 100, 120};
+        int weight[] = {10, 20, 30};
+        int capacity = 50;
     }
 }
