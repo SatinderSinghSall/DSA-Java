6edfc00eae5be65babc91e4c3a3d04474168da62
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-26 21:49:21 +0530

    DSA: Greedy Algorithms - Greedy Algorithm Problem - Minimum Length Chain of Pairs.

---
+++ 31. Greedy Algorithms/src/MaxLenChainPairs.java
@@ -0,0 +1,7 @@
+// Greedy Algorithm Problem - Minimum Length Chain of Pairs.
+
+public class MaxLenChainPairs {
+    public static void main(String[] args) {
+        System.out.println("Greedy Algorithm Problem - Minimum Length Chain of Pairs.");
+    }
+}
