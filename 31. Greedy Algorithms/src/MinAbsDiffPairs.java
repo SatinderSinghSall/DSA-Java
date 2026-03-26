74b68484a268a8ec931c5fd0bd0389e3b68be426
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-26 21:25:45 +0530

    DSA: Greedy Algorithms - Greedy Algorithm Problem - Minimum Absolute Difference Pairs.

---
+++ 31. Greedy Algorithms/src/MinAbsDiffPairs.java
@@ -0,0 +1,7 @@
+// Greedy Algorithm Problem - Minimum Absolute Difference Pairs.
+
+public class MinAbsDiffPairs {
+    public static void main(String[] args) {
+        System.out.println("Greedy Algorithm Problem - Minimum Absolute Difference Pairs.");
+    }
+}
