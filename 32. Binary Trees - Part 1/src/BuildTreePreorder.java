85f274a21e922577726b5f9b96cb15b8b5089137
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-27 19:41:30 +0530

    DSA: Binary Trees Data Structure - Binary Trees: Build Tree Preorder.

--- 32. Binary Trees - Part 1/src/BuildTreePreorder.java
+++ 32. Binary Trees - Part 1/src/BuildTreePreorder.java
@@ -3,5 +3,7 @@
 public class BuildTreePreorder {
     public static void main(String[] args) {
         System.out.println("Binary Trees: Build Tree Preorder.");
+
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
     }
 }
