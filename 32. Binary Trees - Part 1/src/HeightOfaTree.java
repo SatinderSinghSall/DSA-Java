ff662a22d57fc1e2ccb578fb2413519b34b28c69
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-28 21:26:25 +0530

    DSA: Binary Trees Data Structure - Binary Trees: Height of a Tree Implementation.

---
+++ 32. Binary Trees - Part 1/src/HeightOfaTree.java
@@ -0,0 +1,7 @@
+// Binary Trees: Height of a Tree.
+
+public class HeightOfaTree {
+    public static void main(String[] args) {
+        System.out.println("Binary Trees: Height of a Tree.");
+    }
+}
