e9f757c92fe580bcad4cff1081a51b9483a6faa1
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-28 22:39:21 +0530

    DSA: Binary Trees Data Structure - Binary Trees: Height of a Tree Implementation.

--- 32. Binary Trees - Part 1/src/HeightOfaTree.java
+++ 32. Binary Trees - Part 1/src/HeightOfaTree.java
@@ -1,10 +1,11 @@
 // Binary Trees: Height of a Tree.
 
 public class HeightOfaTree {
+
     static class Node {
         int data;
-        LevelOrderTraversal.Node left;
-        LevelOrderTraversal.Node right;
+        Node left;
+        Node right;
 
         Node (int data) {
             this.data = data;
@@ -16,14 +17,14 @@ public class HeightOfaTree {
     static class BinaryTree {
         static int index = -1;
 
-        public static LevelOrderTraversal.Node buildTree(int nodes[]) {
+        public static Node buildTree(int nodes[]) {
             index++;
 
             if (nodes[index] == -1) {
                 return null;
             }
 
-            LevelOrderTraversal.Node newNode = new LevelOrderTraversal.Node(nodes[index]);
+            Node newNode = new Node(nodes[index]);
 
             newNode.left = buildTree(nodes);
             newNode.right = buildTree(nodes);
@@ -32,14 +33,27 @@ public class HeightOfaTree {
         }
     }
 
+    // Function to calculate height
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
     public static void main(String[] args) {
         System.out.println("Binary Trees: Height of a Tree.");
 
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
 
-        LevelOrderTraversal.BinaryTree tree = new LevelOrderTraversal.BinaryTree();
-        LevelOrderTraversal.Node root = tree.buildTree(nodes);
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
 
-        System.out.println(root.data);
+        System.out.println("Root: " + root.data);
+        System.out.println("Height of tree: " + height(root));
     }
 }
