005e22e151191112484ab9916f802956ce451697
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-28 22:30:31 +0530

    DSA: Binary Trees Data Structure - Binary Trees: Sum of Nodes of a Tree Implementation.

---
+++ 32. Binary Trees - Part 1/src/SumOfNodes.java
@@ -0,0 +1,45 @@
+// Binary Trees: Sum of Nodes of a Tree.
+
+public class SumOfNodes {
+    static class Node {
+        int data;
+        LevelOrderTraversal.Node left;
+        LevelOrderTraversal.Node right;
+
+        Node (int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int index = -1;
+
+        public static LevelOrderTraversal.Node buildTree(int nodes[]) {
+            index++;
+
+            if (nodes[index] == -1) {
+                return null;
+            }
+
+            LevelOrderTraversal.Node newNode = new LevelOrderTraversal.Node(nodes[index]);
+
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Binary Trees: Sum of Nodes of a Tree.");
+
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+
+        LevelOrderTraversal.BinaryTree tree = new LevelOrderTraversal.BinaryTree();
+        LevelOrderTraversal.Node root = tree.buildTree(nodes);
+
+        System.out.println(root.data);
+    }
+}
