2362d0d339f36514e0d9e5a8a751643f8a9c823d
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-29 19:52:23 +0530

    DSA: Binary Trees Data Structure - Binary Tree: Diameter Of a Tree Implementation.

--- 33. Binary Trees - Part 2/src/DiameterOfaTree.java
+++ 33. Binary Trees - Part 2/src/DiameterOfaTree.java
@@ -1,6 +1,7 @@
 // Binary Tree: Diameter Of a Tree.
 
 public class DiameterOfaTree {
+
     static class Node {
         int data;
         Node left;
@@ -17,7 +18,7 @@ public class DiameterOfaTree {
         static int index = -1;
 
         public static Node buildTree(int nodes[]) {
-            index ++;
+            index++;
 
             if (nodes[index] == -1) {
                 return null;
@@ -32,6 +33,35 @@ public class DiameterOfaTree {
         }
     }
 
+    // HEIGHT FUNCTION
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int lh = height(root.left);
+        int rh = height(root.right);
+
+        return Math.max(lh, rh) + 1;
+    }
+
+    // DIAMETER FUNCTION (O(n^2))
+    public static int diameter(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDiameter = diameter(root.left);
+        int rightDiameter = diameter(root.right);
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        int selfDiameter = leftHeight + rightHeight + 1;
+
+        return Math.max(selfDiameter, Math.max(leftDiameter, rightDiameter));
+    }
+
     public static void main(String[] args) {
         System.out.println("Binary Tree: Diameter Of a Tree.");
 
@@ -40,6 +70,6 @@ public class DiameterOfaTree {
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
 
-        System.out.println(root.data);
+        System.out.println("Diameter = " + diameter(root));
     }
 }
