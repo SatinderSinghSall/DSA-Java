c4c98251630c366317e70555b434df38ddff5a7e
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-02 20:51:47 +0530

    DSA: Heaps Data Structure / Priority Queue Data Structure : Heaps / Priority Queue: Priority Queue for Objects.

--- 37. Heaps/src/PriorityQueue_Objects.java
+++ 37. Heaps/src/PriorityQueue_Objects.java
@@ -4,18 +4,33 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PriorityQueue_Objects {
+    static class Student implements Comparable<Student> {
+        String name;
+        int rank;
+
+        public Student(String name, int rank) {
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override
+        public int compareTo(Student s2) {
+            return this.rank - s2.rank;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Heaps / Priority Queue: Priority Queue for Objects.");
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Student> pq = new PriorityQueue<>();
 
-        pq.add(3); // Time Complexity: O(logn)
-        pq.add(4);
-        pq.add(1);
-        pq.add(7);
+        pq.add(new Student("A", 4)); // Time Complexity: O(logn)
+        pq.add(new Student("B", 5));
+        pq.add(new Student("C", 2));
+        pq.add(new Student("d", 12));
 
         while (!pq.isEmpty()) {
-            System.out.print(pq.peek() + " "); // Time Complexity: O(1)
+            System.out.print(pq.peek().name + "-" + pq.peek().rank + " "); // Time Complexity: O(1)
             pq.remove(); // Time Complexity: O(logn)
         }
     }
