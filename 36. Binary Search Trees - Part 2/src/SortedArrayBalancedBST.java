4b319480ed5ad3e3f8e2ad01b61d80fe2687ebb3
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-02 15:19:16 +0530

    DSA: Binary Search Trees (BST) Data Structure - Binary Search Tree: Binary Search Tree: Sorted Array to Balanced Binary Search Tree.

--- 36. Binary Search Trees - Part 2/src/SortedArrayBalancedBST.java
+++ 36. Binary Search Trees - Part 2/src/SortedArrayBalancedBST.java
@@ -1,7 +1,7 @@
 // Binary Search Tree: Sorted Array to Balanced Binary Search Tree.
 
 public class SortedArrayBalancedBST {
-    static class  Node {
+    static class Node {
         int data;
         Node left;
         Node right;
@@ -21,9 +21,27 @@ public class SortedArrayBalancedBST {
         preorder(root.right);
     }
 
+    public static Node createBST(int arr[], int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int mid = (start + end) / 2;
+        Node root = new Node(arr[mid]);
+
+        root.left = createBST(arr, start, mid - 1);
+        root.right = createBST(arr, mid + 1, end);
+
+        return root;
+    }
+
     public static void main(String[] args) {
         System.out.println("Binary Search Tree: Sorted Array to Balanced Binary Search Tree.");
 
         int arr[] = {3, 5, 6, 8, 10, 11, 12};
+
+        Node root = createBST(arr, 0, arr.length - 1);
+
+        preorder(root);
     }
 }
