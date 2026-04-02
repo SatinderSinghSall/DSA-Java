402adcc49aa28812d7abf664fe27f4333a203ec5
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-02 20:33:35 +0530

    DSA: Heaps Data Structure / Priority Queue Data Structure : Heaps / Priority Queue: Priority Queue Java Collection Framework.

--- 37. Heaps/src/PriorityQueue_JCF.java
+++ 37. Heaps/src/PriorityQueue_JCF.java
@@ -1,7 +1,22 @@
 // Heaps / Priority Queue: Priority Queue Java Collection Framework.
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class PriorityQueue_JCF {
     public static void main(String[] args) {
         System.out.println("Heaps / Priority Queue: Priority Queue Java Collection Framework.");
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(3); // Time Complexity: O(logn)
+        pq.add(4);
+        pq.add(1);
+        pq.add(7);
+
+        while (!pq.isEmpty()) {
+            System.out.print(pq.peek() + " "); // Time Complexity: O(1)
+            pq.remove(); // Time Complexity: O(logn)
+        }
     }
 }
