e1f5a2f3c97f8c0477df8fca89dddcd60056f5a2
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-02 15:21:55 +0530

    DSA: Binary Search Trees (BST) Data Structure - Binary Search Tree: Binary Search Tree: Convert a BST to a Balanced BST.

---
+++ 36. Binary Search Trees - Part 2/src/ConvertBSTtoBalancedBST.java
@@ -0,0 +1,7 @@
+// Binary Search Tree: Convert a BST to a Balanced BST.
+
+public class ConvertBSTtoBalancedBST {
+    public static void main(String[] args) {
+        System.out.println("Binary Search Tree: Convert a BST to a Balanced BST.");
+    }
+}
