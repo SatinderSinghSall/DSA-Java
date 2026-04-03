da664b1c27f4e07275c15c9fda08899ef0690a11
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-03 20:25:59 +0530

    DSA: Hashing Data Structure - Hashing: HashMap Operations.

--- 38. Hashing/src/HashMap_Operations.java
+++ 38. Hashing/src/HashMap_Operations.java
@@ -1,7 +1,33 @@
 // Hashing: HashMap Operations.
 
+import java.util.HashMap;
+
 public class HashMap_Operations {
     public static void main(String[] args) {
         System.out.println("Hashing: HashMap Operations.");
+
+        // Creating a HashMap:
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        // Inserting into a HashMap: O(1) Time Complexity
+        hm.put("India", 100);
+        hm.put("China", 150);
+        hm.put("USA", 50);
+
+        System.out.println(hm);
+
+        // Getting an Element from a HashMap: O(1) Time Complexity
+        int population = hm.get("India");
+        System.out.println(population);
+        System.out.println(hm.get("UK")); // null no key present
+
+        // Check if an Element is Present in a HashMap: O(1)
+        System.out.println(hm.containsKey("UK")); // False - not present
+        System.out.println(hm.containsKey("India")); // True - present
+
+        // Removing an Element from a HashMap: O(1)
+        hm.remove("USA");
+
+        System.out.println(hm);
     }
 }
