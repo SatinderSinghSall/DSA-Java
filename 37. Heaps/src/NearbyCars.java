06282b177639417ea5575a5c8506d94cb4b56488
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-03 12:54:19 +0530

    DSA: Heaps Data Structure / Priority Queue Data Structure : Heap Data Structure: Nearby Cars Problems Solution.

---
+++ 37. Heaps/src/NearbyCars.java
@@ -0,0 +1,7 @@
+// Heap Data Structure: Nearby Cars Problems Solution.
+
+public class NearbyCars {
+    public static void main(String[] args) {
+        System.out.println("Heap Data Structure: Nearby Cars Problems Solution.");
+    }
+}
