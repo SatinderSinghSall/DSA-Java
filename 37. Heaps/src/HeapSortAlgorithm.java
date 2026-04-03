ff0ed18530c5c9f562b524df3b7192bae5196b32
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-03 12:37:59 +0530

    DSA: Heaps Data Structure / Priority Queue Data Structure : Heap Data Structure: Heap Sort Algorithm.

---
+++ 37. Heaps/src/HeapSortAlgorithm.java
@@ -0,0 +1,9 @@
+// Heap Data Structure: Heap Sort Algorithm.
+
+public class HeapSortAlgorithm {
+    public static void main(String[] args) {
+        System.out.println("Heap Data Structure: Heap Sort Algorithm.");
+
+        int arr[] = {1, 2, 4, 5, 3};
+    }
+}
