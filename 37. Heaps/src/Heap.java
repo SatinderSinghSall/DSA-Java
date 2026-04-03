71d60c1100b30354e8d88fe8c8ee66fe4642ce38
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-03 09:30:49 +0530

    DSA: Heaps Data Structure / Priority Queue Data Structure : Heap Data Structure: Heap Implementation.

---
+++ 37. Heaps/src/Heap.java
@@ -0,0 +1,7 @@
+// Heap Data Structure: Heap Implementation.
+
+public class Heap {
+    public static void main(String[] args) {
+        System.out.println("Heap Data Structure: Heap Implementation.");
+    }
+}
