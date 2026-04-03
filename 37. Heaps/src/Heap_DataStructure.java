b980107047e4a3b3c31d48cd9d736505ff93c5b7
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-03 12:33:44 +0530

    DSA: Heaps Data Structure / Priority Queue Data Structure : Heap Data Structure: Heap Implementation -- Method to add a data in Heap, Method to peek data from Heap, Method to delete data from Heap (3 step process), Method to check Heap is Empty.

--- 37. Heaps/src/Heap_DataStructure.java
+++ 37. Heaps/src/Heap_DataStructure.java
@@ -19,6 +19,9 @@ public class Heap_DataStructure {
                 int temp = arr.get(x);
                 arr.set(x, arr.get(par));
                 arr.set(par, temp);
+
+                x = par;
+                par = (x - 1) / 2;
             }
         }
 
@@ -27,10 +30,68 @@ public class Heap_DataStructure {
             return arr.get(0);
         }
 
+        // Heapify Method:
+        private void heapify(int idx) {
+            int left = 2 * idx + 1;
+            int right = 2 * idx + 2;
+            int minIdx = idx;
+
+            if (left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+                minIdx = left;
+            }
+
+            if (right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+                minIdx = right;
+            }
+
+            if (minIdx != idx) {
+                // Swap:
+                int temp = arr.get(idx);
+                arr.set(idx, arr.get(minIdx));
+                arr.set(minIdx, temp);
+
+                heapify(idx);
+            }
+        }
+
         // Method to delete data from Heap:
+        public int remove() {
+            int data = arr.get(0);
+            int index = 0;
+
+            // Step - 1: Swap 1st & last element:
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size() - 1));
+            arr.set(arr.size() - 1, temp);
+
+            // Step - 2: Delete last element:
+            arr.remove(arr.size() - 1);
+
+            // Step - 3: Heapify Method:
+            heapify(index);
+
+            return data;
+        }
+
+        // Method to check Heap is Empty:
+        private boolean isEmpty() {
+            return arr.size() == 0;
+        }
     }
 
     public static void main(String[] args) {
         System.out.println("Heap Data Structure: Heap Implementation.");
+
+        Heap h = new Heap();
+
+        h.add(3);
+        h.add(4);
+        h.add(1);
+        h.add(5);
+
+        while (!h.isEmpty()) {
+            System.out.print(h.peek() + " ");
+            h.remove();
+        }
     }
 }
