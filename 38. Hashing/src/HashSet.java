2b96019d99b4ea3e65ff725891858cd5e59d69e9
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-04 20:26:36 +0530

    DSA: Hashing Data Structure - Hashing: HashSet Data Structure.

--- 38. Hashing/src/HashSet.java
+++ 38. Hashing/src/HashSet.java
@@ -1,7 +1,7 @@
-// Hashing: HashSet.
+// Hashing: HashSet Data Structure.
 
 public class HashSet {
     public static void main(String[] args) {
-        System.out.println("Hashing: HashSet.");
+        System.out.println("Hashing: HashSet Data Structure.");
     }
 }
