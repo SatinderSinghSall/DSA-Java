4215832f0df3f85fed75d93da45e579b11c69d41
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-05 19:27:57 +0530

    DSA: Hashing Data Structure - Hashing: Count Distinct Elements.

--- 38. Hashing/src/CountDistinctElements.java
+++ 38. Hashing/src/CountDistinctElements.java
@@ -1,7 +1,25 @@
 // Hashing: Count Distinct Elements.
 
+import java.util.HashSet;
+
 public class CountDistinctElements {
     public static void main(String[] args) {
         System.out.println("Hashing: Count Distinct Elements.");
+
+        int[] arr = {10, 20, 20, 30, 40, 10, 50};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        System.out.println();
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+
+        System.out.println();
+        for (int num : arr) {
+            set.add(num); // duplicates automatically ignored
+        }
+
+        System.out.println("Number of distinct elements: " + set.size());
     }
 }
