7229c08b40edd34826c5753bdf8aa3085a6dc997
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-05 19:23:01 +0530

    DSA: Hashing Data Structure - Hashing: Count Distinct Elements.

---
+++ 38. Hashing/src/CountDistinctElements.java
@@ -0,0 +1,7 @@
+// Hashing: Count Distinct Elements.
+
+public class CountDistinctElements {
+    public static void main(String[] args) {
+        System.out.println("Hashing: Count Distinct Elements.");
+    }
+}
