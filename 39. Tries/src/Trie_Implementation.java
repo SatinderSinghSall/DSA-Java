6c52fbddd699670a9e012ba54d126aa80d322e93
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-07 21:46:00 +0530

    DSA: Tries Data Structure - Trie: Trie Data Structure Implementation -- Time Complexity - O(L) where L is the 'Length of the largest word'.

--- 39. Tries/src/Trie_Implementation.java
+++ 39. Tries/src/Trie_Implementation.java
@@ -33,6 +33,23 @@ public class Trie_Implementation {
         curr.endOfWord = true;
     }
 
+    // Method to Search in a Trie: Time Complexity - O(L) where L is the 'Length of the largest word'.
+    public static boolean search(String key) {
+        Node curr = root;
+
+        for (int i = 0; i < key.length(); i++) {
+            int idx = key.charAt(i) - 'a';
+
+            if (curr.children[idx] == null) {
+                return false;
+            }
+
+            curr = curr.children[idx];
+        }
+
+        return curr.endOfWord;
+    }
+
     public static void main(String[] args) {
         System.out.println("Trie: Trie Data Structure Implementation.");
 
@@ -41,5 +58,10 @@ public class Trie_Implementation {
         for (int i = 0; i < words.length; i ++) {
             insert(words[i]);
         }
+
+        System.out.println(search("there")); // true
+        System.out.println(search("their")); // true
+        System.out.println(search("thor"));  // false
+        System.out.println(search("an"));    // false
     }
 }
