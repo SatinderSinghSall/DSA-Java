8af71789e1612094f8392368db0c1f208f205f36
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-07 21:53:18 +0530

    DSA: Tries Data Structure - Trie: Word Break Problem & Prefix Problem.

---
+++ 39. Tries/src/Prefix_Problem.java
@@ -0,0 +1,7 @@
+// Trie: Prefix Problem.
+
+public class Prefix_Problem {
+    public static void main(String[] args) {
+        System.out.println("Trie: Prefix Problem.");
+    }
+}
---
+++ 39. Tries/src/WordBreak_Problem.java
@@ -0,0 +1,7 @@
+// Trie: Word Break Problem.
+
+public class WordBreak_Problem {
+    public static void main(String[] args) {
+        System.out.println("Trie: Word Break Problem.");
+    }
+}
