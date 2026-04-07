db819000f79ce083a5f0b01981c53342c79a901c
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-07 21:41:10 +0530

    DSA: Tries Data Structure - Trie: Trie Data Structure Implementation -- Inserting en Element into a Trie: Time Complexity - O(L) where L is the 'Length of the largest word'.

--- 39. Tries/src/Trie_Implementation.java
+++ 39. Tries/src/Trie_Implementation.java
@@ -5,7 +5,7 @@ public class Trie_Implementation {
     // Creating a Trie:
     static class Node {
         Node children[] = new Node[26];
-        boolean endOfNode = false;
+        boolean endOfWord = false;
 
         Node() {
             for (int i = 0; i < 26; i ++) {
@@ -16,9 +16,30 @@ public class Trie_Implementation {
 
     public static Node root = new Node();
 
+    // Inserting en Element into a Trie: Time Complexity - O(L) where L is the 'Length of the largest word'.
+    public static void insert(String word) {
+        Node curr = root;
+
+        for (int level = 0; level < word.length(); level ++) {
+            int idx = word.charAt(level) - 'a';
+
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+
+            curr = curr.children[idx];
+        }
+
+        curr.endOfWord = true;
+    }
+
     public static void main(String[] args) {
         System.out.println("Trie: Trie Data Structure Implementation.");
 
         String words[] = {"the", "a", "there", "their", "any", "there"};
+
+        for (int i = 0; i < words.length; i ++) {
+            insert(words[i]);
+        }
     }
 }
