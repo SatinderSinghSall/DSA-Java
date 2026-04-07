a80035341bb79284700032b94367a6eb8d44b17e
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-07 21:23:52 +0530

    DSA: Tries Data Structure - Trie: Trie Data Structure Implementation -- Creating a Trie.

--- 39. Tries/src/Trie_Implementation.java
+++ 39. Tries/src/Trie_Implementation.java
@@ -1,7 +1,24 @@
 // Trie: Trie Data Structure Implementation.
 
 public class Trie_Implementation {
+
+    // Creating a Trie:
+    static class Node {
+        Node children[] = new Node[26];
+        boolean endOfNode = false;
+
+        Node() {
+            for (int i = 0; i < 26; i ++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
     public static void main(String[] args) {
         System.out.println("Trie: Trie Data Structure Implementation.");
+
+        String words[] = {"the", "a", "there", "their", "any", "there"};
     }
 }
