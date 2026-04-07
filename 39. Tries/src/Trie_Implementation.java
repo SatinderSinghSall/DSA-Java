b6a8e457c73742d6cb3e03e0e56646fa001c2786
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-07 21:19:43 +0530

    DSA: Tries Data Structure - Trie: Trie Data Structure Implementation.

---
+++ 39. Tries/src/Trie_Implementation.java
@@ -0,0 +1,7 @@
+// Trie: Trie Data Structure Implementation.
+
+public class Trie_Implementation {
+    public static void main(String[] args) {
+        System.out.println("Trie: Trie Data Structure Implementation.");
+    }
+}
