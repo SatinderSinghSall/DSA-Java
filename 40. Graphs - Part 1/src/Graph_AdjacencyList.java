a35b25fb64bd15227b1b3681095d260ddd087ee9
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-12 18:44:18 +0530

    DSA: Graph Data Structure - Graphs: Graph Implementation using Adjacency List.

---
+++ 40. Graphs - Part 1/src/Graph_AdjacencyList.java
@@ -0,0 +1,7 @@
+// Graphs: Graph Implementation using Adjacency List.
+
+public class Graph_AdjacencyList {
+    public static void main(String[] args) {
+        System.out.println("Graphs: Graph Implementation using Adjacency List.");
+    }
+}
