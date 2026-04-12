73b6c8271c146f360d061bcd01a6ad8defe036d5
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-12 21:08:48 +0530

    DSA: Graph Data Structure - Graphs: Has Path using DFS

---
+++ 40. Graphs - Part 1/src/HasPath_DFS.java
@@ -0,0 +1,96 @@
+// Graphs: Has Path using DFS
+
+/*
+Graph Structure
+        1 -------- 3
+       /           | \
+      /            |  \
+     0             |   5 -------- 6
+      \            |  /
+       \           | /
+        2 -------- 4
+*/
+
+import java.util.ArrayList;
+
+public class HasPath_DFS {
+
+    static class Edge {
+        int source;
+        int destination;
+        int weight;
+
+        public Edge(int source, int destination, int weight) {
+            this.source = source;
+            this.destination = destination;
+            this.weight = weight;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge>[] graph) {
+
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // 0
+        graph[0].add(new Edge(0,1,1));
+        graph[0].add(new Edge(0,2,1));
+
+        // 1
+        graph[1].add(new Edge(1,0,1));
+        graph[1].add(new Edge(1,3,1));
+
+        // 2
+        graph[2].add(new Edge(2,0,1));
+        graph[2].add(new Edge(2,4,1));
+
+        // 3
+        graph[3].add(new Edge(3,1,1));
+        graph[3].add(new Edge(3,4,1));
+        graph[3].add(new Edge(3,5,1));
+
+        // 4
+        graph[4].add(new Edge(4,2,1));
+        graph[4].add(new Edge(4,3,1));
+        graph[4].add(new Edge(4,5,1));
+
+        // 5
+        graph[5].add(new Edge(5,3,1));
+        graph[5].add(new Edge(5,4,1));
+        graph[5].add(new Edge(5,6,1));
+
+        // 6
+        graph[6].add(new Edge(6,5,1));
+    }
+
+    // Has Path using DFS
+    public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean vis[]) {
+
+        if(src == dest){
+            return true;
+        }
+
+        vis[src] = true;
+
+        for(int i = 0; i < graph[src].size(); i++){
+            Edge e = graph[src].get(i);
+
+            if(!vis[e.destination] && hasPath(graph, e.destination, dest, vis)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Graphs: Has Path using DFS.");
+
+        int V = 7;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+
+        System.out.println(hasPath(graph, 0, 5, new boolean[V]));
+    }
+}
