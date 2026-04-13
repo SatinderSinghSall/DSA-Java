7df1dd6378c7e733966cd36d87ade1ca83f1b8e6
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-13 20:30:50 +0530

    DSA: Graph Data Structure - Graphs: Connected Components.

---
+++ 41. Graphs - Part 2/src/ConnectedComponents.java
@@ -0,0 +1,132 @@
+// Graphs: Connected Components.
+
+/*
+Graph Structure:
+        1 -------- 3
+       /           | \
+      /            |  \
+     0             |   5 -------- 6
+      \            |  /
+       \           | /
+        2 -------- 4
+*/
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ConnectedComponents {
+    static class Edge {
+        int source;
+        int destination;
+        int weight;
+
+        public Edge(int source, int destination, int weight) {
+            this.source = source;
+            this.destination = destination;
+            this.weight = weight;
+        }
+    }
+
+    public static void  createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i ++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // 0 - Vertex:
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+
+        // 1 - Vertex:
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+
+        // 2 - Vertex:
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+
+        // 3 - Vertex:
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+
+        // 4 - Vertex:
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+
+        // 5 - Vertex:
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+
+        // 6 - Vertex:
+        graph[6].add(new Edge(6, 5, 1));
+    }
+
+    // Method for Breath First Search Algorithm: BFS
+    public static void BFS_Algorithm(ArrayList<Edge>[] graph) {
+        boolean vis[] = new boolean[graph.length];
+
+        for (int i = 0; i < graph.length; i ++) {
+            if (!vis[i]) {
+                BFS_Util_Algorithm(graph, vis);
+            }
+        }
+    }
+
+    // Method for Breath First Search Algorithms HELPER util method: BFS
+    public static void BFS_Util_Algorithm(ArrayList<Edge>[] graph, boolean vis[]) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0); // source = 0
+
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+
+            if (!vis[curr]) { // visit -> curr
+                System.out.print(curr + " ");
+                vis[curr] = true;
+
+                for (int i = 0; i < graph[curr].size(); i ++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.destination);
+                }
+            }
+        }
+    }
+
+    // Method for Depth First Search Algorithm:
+    public static void DFS_Algorithm(ArrayList<Edge>[] graph) {
+        boolean vis[] = new boolean[graph.length];
+
+        for (int i = 0; i < graph.length; i ++) {
+            DFS_Util_Algorithm(graph, i, vis);
+        }
+    }
+
+    // Method for Depth First Search Algorithm HELPER Util method: DFS
+    public static void DFS_Util_Algorithm(ArrayList<Edge>[] graph, int curr, boolean vis[]) {
+
+        System.out.print(curr + " ");
+        vis[curr] = true;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+
+            if (!vis[e.destination]) {
+                DFS_Util_Algorithm(graph, e.destination, vis);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Graphs: Depth First Search Algorithm. (DFS)");
+
+        int V = 7;
+
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+        boolean vis[] = new boolean[V];
+//        BFS_Algorithm(graph, 0, vis);
+    }
+}
