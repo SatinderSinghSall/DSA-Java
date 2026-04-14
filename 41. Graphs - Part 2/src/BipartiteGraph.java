450d5190ad678170913b68fa4031ef12a3f899e5
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-14 14:22:29 +0530

    DSA: Graph Data Structure - Graph: Bipartite Graph.

---
+++ 41. Graphs - Part 2/src/BipartiteGraph.java
@@ -0,0 +1,68 @@
+// Graph: Bipartite Graph.
+
+/*
+
+Graph: Bipartite Graph.
+
+Graph Structure:
+
+      0
+     / \
+    /   \
+   1     2
+   |     |
+   |     |
+   3 --- 4
+
+Edges:
+0 -- 1
+0 -- 2
+1 -- 3
+2 -- 4
+3 -- 4
+
+*/
+
+import java.util.ArrayList;
+
+public class BipartiteGraph {
+    static class Edge {
+        int src;
+        int desc;
+
+        public Edge(int s, int d) {
+            this.desc = d;
+            this.src = s;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i ++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Graph: Bipartite Graph.");
+
+        int v = 5;
+
+        ArrayList<Edge> graph[] = new ArrayList[v];
+        createGraph(graph);
+    }
+}
