750d084ec9bc942f143b8f6497ddcf80751b70af
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-14 15:58:02 +0530

    DSA: Graph Data Structure - Graphs: Topological Sorting.

---
+++ 41. Graphs - Part 2/src/TopologicalSorting.java
@@ -0,0 +1,7 @@
+// Graphs: Topological Sorting.
+
+public class TopologicalSorting {
+    public static void main(String[] args) {
+        System.out.println("Graphs: Topological Sorting.");
+    }
+}
