d8cd5e202fd5ff3fac19c1fbc3f5bd139b22d8f6
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-14 20:37:46 +0530

    DSA: Graph Data Structure - Graphs: Topological Sorting.

--- 41. Graphs - Part 2/src/TopologicalSorting.java
+++ 41. Graphs - Part 2/src/TopologicalSorting.java
@@ -16,12 +16,12 @@
 // Edges:
 // 5 -> 0
 // 5 -> 2
-// 4 -> 0
-// 4 -> 1
+// 4 -> 0// 4 -> 1
 // 2 -> 3
 // 3 -> 1
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class TopologicalSorting {
     static class Edge {
@@ -50,6 +50,37 @@ public class TopologicalSorting {
         graph[5].add(new Edge(5, 2));
     }
 
+    // Method for Topological Sorting Helper Utility in a Graph.
+    public static void topologicalSortingUtil(ArrayList<Edge>[] graph, int curr, boolean vis[], Stack<Integer> stack) {
+        vis[curr] = true;
+
+        for (int i = 0; i < graph[curr].size(); i ++) {
+            Edge e = graph[curr].get(i);
+
+            if (!vis[e.dest]) {
+                topologicalSortingUtil(graph, e.dest, vis, stack);
+            }
+        }
+
+        stack.push(curr);
+    }
+
+    // Method for Topological Sorting in a Graph: Time Complexity - O(V+E)
+    public static void topologicalSorting(ArrayList<Edge>[] graph) {
+        boolean vis[] = new boolean[graph.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < graph.length; i ++) {
+            if (!vis[i]) {
+                topologicalSortingUtil(graph, i, vis, stack); // Modified DFS Method.
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Graphs: Topological Sorting.");
 
@@ -57,5 +88,7 @@ public class TopologicalSorting {
 
         ArrayList<Edge> graph[] = new ArrayList[v];
         createGraph(graph);
+
+        topologicalSorting(graph);
     }
 }
