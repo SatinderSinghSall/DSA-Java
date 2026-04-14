ec3503552e8a14fcb891ad7ee5930542a2709cd6
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-14 19:52:26 +0530

    DSA: Graph Data Structure - Graphs: Topological Sorting.

--- 41. Graphs - Part 2/src/TopologicalSorting.java
+++ 41. Graphs - Part 2/src/TopologicalSorting.java
@@ -1,7 +1,61 @@
 // Graphs: Topological Sorting.
 
+// Graph Structure:
+//
+//        5           4
+//       / \         / \
+//      v   v       v   v
+//      2   0       0   1
+//      |
+//      v
+//      3
+//      |
+//      v
+//      1
+//
+// Edges:
+// 5 -> 0
+// 5 -> 2
+// 4 -> 0
+// 4 -> 1
+// 2 -> 3
+// 3 -> 1
+
+import java.util.ArrayList;
+
 public class TopologicalSorting {
+    static class Edge {
+        int src;
+        int dest;
+
+        Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i ++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[2].add(new Edge(2, 3));
+
+        graph[3].add(new Edge(3, 1));
+
+        graph[4].add(new Edge(4, 0));
+        graph[4].add(new Edge(4, 1));
+
+        graph[5].add(new Edge(5, 0));
+        graph[5].add(new Edge(5, 2));
+    }
+
     public static void main(String[] args) {
         System.out.println("Graphs: Topological Sorting.");
+
+        int v = 6;
+
+        ArrayList<Edge> graph[] = new ArrayList[v];
+        createGraph(graph);
     }
 }
