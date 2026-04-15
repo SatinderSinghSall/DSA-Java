0a4b9ac67026ac30b84d4e8530ba2124cb8c956b
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-15 20:25:24 +0530

    DSA: Graphs: Bellman Ford Algorithm.

---
+++ 43. Graphs - Part 4/src/BellmanFordAlgorithm.java
@@ -0,0 +1,93 @@
+// Graphs: Bellman Ford Algorithm.
+
+import java.util.ArrayList;
+
+public class BellmanFordAlgorithm {
+
+    public static class Edge {
+
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[]) {
+
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
+
+        graph[1].add(new Edge(1, 2, -4));
+
+        graph[2].add(new Edge(2, 3, 2));
+
+        graph[3].add(new Edge(3, 4, 4));
+
+        graph[4].add(new Edge(4, 1, -1));
+    }
+
+    // Method to find Bellman Ford Algorithm
+    public static void BellmanFordAlgorithm_Logic(ArrayList<Edge> graph[], int src) {
+
+        int dist[] = new int[graph.length];
+
+        for (int i = 0; i < dist.length; i++) {
+            if (i != src) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+
+        int V = graph.length;
+
+        // Perform relaxation V-1 times
+        for (int i = 0; i < V - 1; i++) {
+
+            for (int j = 0; j < graph.length; j++) {
+
+                for (int k = 0; k < graph[j].size(); k++) {
+
+                    Edge e = graph[j].get(k);
+
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+
+                    // Relaxation step
+                    if (dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]) {
+
+                        dist[v] = dist[u] + wt;
+                    }
+                }
+            }
+        }
+
+        // Print shortest distances
+        for (int i = 0; i < dist.length; i++) {
+            System.out.print(dist[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Graphs: Bellman Ford Algorithm.");
+
+        int V = 5;
+
+        ArrayList<Edge> graph[] = new ArrayList[V];
+
+        createGraph(graph);
+
+        int src = 0;
+
+        BellmanFordAlgorithm_Logic(graph, src);
+    }
+}
