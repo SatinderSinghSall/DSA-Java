77e4f77a29cf28885abe33ffc4d787ab1a3d3a3d
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-17 22:50:52 +0530

    DSA: Dynamic Programming - Dynamic Programming: Given a positive integer n, find the nth Fibonacci number using Memization & Tabulation Approchs.

--- 46. Dynamic Programming - Part 1/src/FibonacciNumber.java
+++ 46. Dynamic Programming - Part 1/src/FibonacciNumber.java
@@ -1,6 +1,8 @@
 // Dynamic Programming: Given a positive integer n, find the nth Fibonacci number.
 
 public class FibonacciNumber {
+
+    // Memoization Method:
     public static int fibonacci(int n, int f[]) {
         if (n == 0 || n == 1) {
             return n;
@@ -15,6 +17,19 @@ public class FibonacciNumber {
         return f[n];
     }
 
+    // Tabulation Method:
+    public static int fibonacciTabulation(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i ++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         System.out.println("Dynamic Programming: Given a positive integer n, find the nth Fibonacci number.");
 
@@ -22,5 +37,6 @@ public class FibonacciNumber {
         int f[] = new int[n + 1];
 
         System.out.println(fibonacci(n, f));
+        System.out.println(fibonacciTabulation(n));
     }
 }
