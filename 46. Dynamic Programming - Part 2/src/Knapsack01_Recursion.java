0b5ca739ef53e12e6356421d9bba2d0dcbdb03d7
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-05-06 22:42:42 +0530

    DSA: Dynamic Programming - Kanpsack Problem - Dynamic Programming: O/1 Knapsack Problem using Recursion.

--- 46. Dynamic Programming - Part 2/src/Knapsack01_Recursion.java
+++ 46. Dynamic Programming - Part 2/src/Knapsack01_Recursion.java
@@ -1,7 +1,32 @@
 // Dynamic Programming: O/1 Knapsack Problem using Recursion.
 
 public class Knapsack01_Recursion {
+    public static int knapsack(int val[], int wt[], int W, int n) {
+        if (W == 0 || n == 0) {
+            return  0;
+        }
+
+        if (wt[n - 1] <= W) { // valid
+            // include
+            int ans = val[n - 1] + knapsack(val, wt, W - wt[n - 1], n - 1);
+
+            // exclude
+            int ans2 = knapsack(val, wt, W, n - 1);
+
+            return Math.max(ans, ans2);
+        } else { // not valid
+            return knapsack(val, wt, W, n - 1);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Dynamic Programming: O/1 Knapsack Problem using Recursion.");
+
+        int val[] = {15, 14, 10, 45, 30};
+        int wt[] = {2, 5, 1, 3, 4};
+        int W = 7;
+        int n = val.length;
+
+        System.out.println(knapsack(val, wt, W, n));
     }
 }
