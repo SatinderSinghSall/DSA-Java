a75c109d5f3e29c5319d4ea6b0e78cb4e2edf2b2
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-05-06 16:11:01 +0530

    DSA: Dynamic Programming - Kanpsack Problem - Dynamic Programming: O/1 Knapsack Problem using Recursion.

---
+++ 46. Dynamic Programming - Part 2/src/Knapsack01_Recursion.java
@@ -0,0 +1,7 @@
+// Dynamic Programming: O/1 Knapsack Problem using Recursion.
+
+public class Knapsack01_Recursion {
+    public static void main(String[] args) {
+        System.out.println("Dynamic Programming: O/1 Knapsack Problem using Recursion.");
+    }
+}
