f11b9fc8994e2417d81780ab903cb7520158315d
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-05-07 09:23:17 +0530

    DSA: Dynamic Programming - Kanpsack Problem - Dynamic Programming: O/1 Knapsack Problem using Tabulation.

--- 46. Dynamic Programming - Part 2/src/Knapsack01_Tabulation.java
+++ 46. Dynamic Programming - Part 2/src/Knapsack01_Tabulation.java
@@ -35,6 +35,13 @@ public class Knapsack01_Tabulation {
             }
         }
 
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+
         return dp[n][W];
     }
 
@@ -45,7 +52,7 @@ public class Knapsack01_Tabulation {
 
         int W = 7;
 
-        System.out.println("Maximum Profit = "
+        System.out.println("\nMaximum Profit = "
                 + knapsack(val, wt, W));
     }
 }
